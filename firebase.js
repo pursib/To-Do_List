ef6f1f99f066304a27d0ba8588ab3f93cbd66819
Preslav <pursib@abv.bg>
2021-08-23 13:00:41 -0700

    Adding firebase database to the project and connect it by javascript

---
+++ firebase.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCudMj-mz5F1-Y3GFKT4f56GCMKZbEHlfI",
+    authDomain: "to-do-list-35af8.firebaseapp.com",
+    projectId: "to-do-list-35af8",
+    storageBucket: "to-do-list-35af8.appspot.com",
+    messagingSenderId: "87950408244",
+    appId: "1:87950408244:web:462bee60e14b33529ee861",
+    measurementId: "G-6LK3QP7T5X"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  var db = firebase.firestore();
+
